98a2065502d11f01b25af3a39f283418b15ea98d
Dominik <frexorplay@gmail.com>
2025-01-12 18:27:37 +0100

    model v0.3

--- credit_scoring.py
+++ credit_scoring.py
@@ -1,16 +1,17 @@
 from joblib import load
 import sklearn
 import pandas as pd
+import xgboost
 
 
 def load_pipeline():
-    loaded_pipeline = load('credit_scoring_pipeline.joblib')
+    loaded_pipeline = load('credit_scoring_pipelinev0_3.joblib')
     return loaded_pipeline
 
 
 def predict(user_data):
     credit_data = pd.DataFrame([user_data])
     pipeline = load_pipeline()
-    pred = pipeline.predict(credit_data)[0]
-    return pred
+    pred = pipeline.predict_proba(credit_data)[:, 0]
+    return round(pred[0], 0)
 
